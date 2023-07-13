3dd47c115bf667a931a2fec5f2b9ec1315ba9cdb
Serem23 <cynthiajerotich@yahoo.com>
2023-07-13 16:37:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  * main -Entry point 
  * Return : Always 0 (succcess)
  */
-int main (void)
-{puts("\"Programming is like building a multilingual puzzle");
-return (0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
