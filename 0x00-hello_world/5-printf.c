5d88b4c53af374c9d56618eb35fe87af7453ea3c
Serem23 <cynthiajerotich@yahoo.com>
2023-07-17 13:30:02 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 /**
  * main - Entry point
  *
- * Return : Always 0 (succcess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        printf("\"Programming is like building a multilingual puzzle");
+        printf("with proper grammar, but the outcome is a piece of art,\");
         return (0);
+}
