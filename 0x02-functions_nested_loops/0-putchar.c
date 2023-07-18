3deeb373635683f164028f39ebf37f5d93c99581
Serem23 <cynthiajerotich@yahoo.com>
2023-07-18 08:19:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<main.h>
+#include <main.h>
 
 /**
   *main - Entry point
@@ -6,7 +6,7 @@
   */
 int main(void)
 {
-	-putchar('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
