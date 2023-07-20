ecbc425b913cbc9c9d940b4b4f6b97051fe98ff3
Serem23 <cynthiajerotich@yahoo.com>
2023-07-20 12:02:19 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,22 +2,29 @@
 
 /**
  *print_square - print square
- *size: size of the square
+ *@size: size of the square
  */
 void print_square(int size)
 {
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	}else
+	{
 		int i, j;
+
 	for (i = 0; i < size; i++)
+	{
 		for (j = 0; j < size; j++)
 		{
 			_putchar('#');
 		}
 		_putchar('\n');
 	}
+	}
 }
+	
+	
+
+
 
