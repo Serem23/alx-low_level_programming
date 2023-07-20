1170099c4de7a6a37a1709376060228485c6dd3b
Serem23 <cynthiajerotich@yahoo.com>
2023-07-20 09:21:43 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
-  *print_diagonal _ draw diagonal lines
-  *@n: number of times the character  \ should be printed
+  *main - print_diagonal _ draw diagonal lines
+  *print_diagonal
+  *Return: Always 0 (success)
   */
 void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}else
+	}
+	else
 	{
 		int i, j;
+
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
-  *print_square - print square
-  *@size: size of the square 
-  */
+ *print_square - print square
+ *size: size of the square
+ */
 void print_square(int size)
 {
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}else
+	}
+	else
 		int i, j;
 	for (i = 0; i < size; i++)
 		for (j = 0; j < size; j++)
